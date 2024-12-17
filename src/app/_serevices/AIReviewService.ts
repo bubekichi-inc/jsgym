08ec7de8992e54a18e841fa8485684c2e4e217b1
rs-akane <rs.akane.y@gmail.com>
2024-12-17 20:50:29 +0900

    クラスの返り値に型指定

--- src/app/_serevices/AIReviewService.ts
+++ src/app/_serevices/AIReviewService.ts
@@ -1,5 +1,6 @@
 import OpenAI from "openai";
 import { Message } from "../_types/Message";
+import { CodeReviewResponse } from "../api/questions/[questionId]/code_review/_types/CodeReview";
 export class AIReviewService {
   private static openai = new OpenAI({
     apiKey: process.env.OPENAI_SECRET_KEY,
@@ -11,7 +12,7 @@ export class AIReviewService {
   }: {
     question: string;
     answer: string;
-  }) {
+  }): Promise<CodeReviewResponse> {
     const message = `コードレビューしてJSON形式で出力してくださいください。1行目はレビュー方針に沿って回答出来ていて、かつ処理が正しいかをboolean型(isCorrect)で, 2行目はレビュー内容のstring型(reviewComment)で一問一答形式ではなく文章として問題点があれば問題点を答えてください。レビューの方針は「①正しく動作すること②1. 引数になる定数の定義 → 2. 関数の定義 → 3. 関数に引数を渡して実行した結果をconsole.logの形になっていること③アロー関数を用いていること④配列メソッド（mapやfilterなど）で書ける部分をfor文などで書いていないこと（可読性のため）④省略記法を用いていること」です。すべての方針に対してコメントする必要はないです。問題点を中心に、完璧ならその旨をコメントしてください。レビュー方針というワードは使ってほしくないです。すべて日本語でお願いします。問題は${question},回答は${answer}`;
 
     const response = await this.openai.chat.completions.create({
--- src/app/api/questions/[questionId]/code_review/route.ts
+++ src/app/api/questions/[questionId]/code_review/route.ts
@@ -20,11 +20,10 @@ export const POST = async (req: NextRequest, { params }: Props) => {
     const { question, answer }: CodeReviewRequest = body;
     const message = `コードレビューしてJSON形式で出力してくださいください。1行目はレビュー方針に沿って回答出来ていて、かつ処理が正しいかをboolean型(isCorrect)で, 2行目はレビュー内容のstring型(reviewComment)で一問一答形式ではなく文章として問題点があれば問題点を答えてください。レビューの方針は「①正しく動作すること②1. 引数になる定数の定義 → 2. 関数の定義 → 3. 関数に引数を渡して実行した結果をconsole.logの形になっていること③アロー関数を用いていること④配列メソッド（mapやfilterなど）で書ける部分をfor文などで書いていないこと（可読性のため）④省略記法を用いていること」です。すべての方針に対してコメントする必要はないです。問題点を中心に、完璧ならその旨をコメントしてください。レビュー方針というワードは使ってほしくないです。すべて日本語でお願いします。問題は${question},回答は${answer}`;
 
-    const { isCorrect, reviewComment }: CodeReviewResponse =
-      await AIReviewService.getCodeReview({
-        question,
-        answer,
-      });
+    const { isCorrect, reviewComment } = await AIReviewService.getCodeReview({
+      question,
+      answer,
+    });
     const status = isCorrect ? StatusType.PASSED : StatusType.REVISION_REQUIRED;
 
     //ステータスを更新する
