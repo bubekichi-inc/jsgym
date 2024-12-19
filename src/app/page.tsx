eeb7d91a9ad3c4fefb28ae68929295c41be0cfa1
shuheitachikawa <shuhei.tusx@gmail.com>
2024-12-19 13:34:27 +0900

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,35 @@
-"use client";
-import { supabase } from "./_utils/supabase";
-import { Button } from "./_components/Button";
-import { useRouter } from "next/navigation";
-import { useSupabaseSession } from "./_hooks/useSupabaseSessoin";
-import { useEffect } from "react";
+'use client'
+import { supabase } from './_utils/supabase'
+import { Button } from './_components/Button'
+import { useRouter } from 'next/navigation'
+import { useSupabaseSession } from './_hooks/useSupabaseSessoin'
+import { useEffect } from 'react'
 
 export default function Home() {
-  const router = useRouter();
-  const { session, isLoading } = useSupabaseSession();
+  const router = useRouter()
+  const { session, isLoading } = useSupabaseSession()
+
   useEffect(() => {
-    if (isLoading) return;
+    if (isLoading) return
     if (session) {
-      router.replace("/courses");
+      router.replace('/courses')
     }
-  }, [session, isLoading]);
+  }, [session, isLoading])
 
   const signIn = async () => {
     try {
       const { error } = await supabase.auth.signInWithOAuth({
-        provider: "google",
+        provider: 'google',
         options: {
           redirectTo: `${process.env.NEXT_PUBLIC_APP_BASE_URL}/oauth/callback/google`,
         },
-      });
-      if (error) throw new Error(error.message);
+      })
+      if (error) throw new Error(error.message)
     } catch (e) {
-      alert(`ログインに失敗しました:${e}`);
-      console.error(e);
+      alert(`ログインに失敗しました:${e}`)
+      console.error(e)
     }
-  };
+  }
 
   return (
     <div className="flex justify-center items-center h-screen">
@@ -36,5 +37,5 @@ export default function Home() {
         はじめる
       </Button>
     </div>
-  );
+  )
 }
