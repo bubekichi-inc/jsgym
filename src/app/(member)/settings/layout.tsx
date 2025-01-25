834dec3d0566377d1895ac31a49c46ae4cc6f013
T.Wada <28886385+TakeshiWada1980@users.noreply.github.com>
2025-01-25 10:09:08 +0900

    Propsを分割代入構文で記述するスタイルに変更

--- src/app/(member)/settings/layout.tsx
+++ src/app/(member)/settings/layout.tsx
@@ -6,8 +6,8 @@ interface Props {
   children: ReactNode;
 }
 
-const Layout: React.FC<Props> = (props) => {
-  return <div className="mx-auto mt-10 w-full max-w-2xl">{props.children}</div>;
+const Layout: React.FC<Props> = ({ children }) => {
+  return <div className="mx-auto mt-10 w-full max-w-2xl">{children}</div>;
 };
 
 export default Layout;
--- src/app/(member)/settings/points/_components/PointPurchaseButton.tsx
+++ src/app/(member)/settings/points/_components/PointPurchaseButton.tsx
@@ -2,21 +2,23 @@ import React from "react";
 
 interface Props {
   amount: number; // ポイント数
-  price: number; // 価格
+  price: number; // 価格 (日本円)
   onClick: (amount: number) => void;
 }
 
-export const PointPurchaseButton: React.FC<Props> = (props) => {
+export const PointPurchaseButton: React.FC<Props> = ({
+  amount,
+  price,
+  onClick,
+}) => {
   return (
     <button
       className="w-28 rounded-md border border-indigo-500 bg-indigo-50 py-3 hover:bg-indigo-100"
-      onClick={() => props.onClick(props.amount)}
+      onClick={() => onClick(amount)}
     >
       <div className="flex flex-col gap-y-1">
-        <div className="text-lg font-bold">
-          {props.amount.toLocaleString()}pt
-        </div>
-        <div className="text-sm">{props.price.toLocaleString()}円</div>
+        <div className="text-lg font-bold">{amount.toLocaleString()}pt</div>
+        <div className="text-sm">{price.toLocaleString()}円</div>
       </div>
     </button>
   );
