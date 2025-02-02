1a0472c34d5c55ce865c70dfa439d656c4e770b3
T.Wada <28886385+TakeshiWada1980@users.noreply.github.com>
2025-02-02 12:10:25 +0900

    update layout.tsx and page.tsx セッティングページの共通部の構成を変更

--- src/app/(member)/settings/points/page.tsx
+++ src/app/(member)/settings/points/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import NextLink from "next/link";
 import React from "react";
 import { PointPurchaseButton } from "./_components/PointPurchaseButton";
 import { PointPackage } from "@/app/_constants/productMaster";
@@ -33,54 +32,33 @@ const Page: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col gap-y-6">
-      <h2 className="text-3xl font-bold">各種設定</h2>
-      <div className="flex gap-x-12">
-        <NextLink
-          href="/settings/profile"
-          className="px-6 pb-1 text-xl font-bold text-gray-500"
-        >
-          プロフィール
-        </NextLink>
-        <div className="border-b-2 border-black pb-1">
-          <h3 className="px-6 text-xl font-bold">ポイント購入</h3>
+    <div className="flex flex-col gap-y-12">
+      <div className="flex items-center justify-between">
+        <h4>保有ポイント</h4>
+        <div className="flex items-end gap-x-2">
+          {data ? (
+            <div className="text-3xl font-bold">{data.points}</div>
+          ) : (
+            <div className="text-3xl text-gray-500">...取得中...</div>
+          )}
+          <div className="font-bold">pt</div>
         </div>
-        <NextLink
-          href="#"
-          className="px-6 pb-1 text-xl font-bold text-gray-500"
-        >
-          通知設定
-        </NextLink>
       </div>
-
-      <div className="mt-10 flex flex-col gap-y-12">
-        <div className="flex items-center justify-between">
-          <h4>保有ポイント</h4>
-          <div className="flex items-end gap-x-2">
-            {data ? (
-              <div className="text-3xl font-bold">{data.points}</div>
-            ) : (
-              <div className="text-3xl text-gray-500">...取得中...</div>
-            )}
-            <div className="font-bold">pt</div>
-          </div>
+      <div className="flex flex-col gap-y-3">
+        <h4>ポイント購入</h4>
+        <div className="flex gap-x-3">
+          {Object.values(PointPackage).map((pointPackage) => (
+            <PointPurchaseButton
+              key={pointPackage}
+              pointPackage={pointPackage}
+              onClick={purchasePoints}
+            />
+          ))}
         </div>
-        <div className="flex flex-col gap-y-3">
-          <h4>ポイント購入</h4>
-          <div className="flex gap-x-3">
-            {Object.values(PointPackage).map((pointPackage) => (
-              <PointPurchaseButton
-                key={pointPackage}
-                pointPackage={pointPackage}
-                onClick={purchasePoints}
-              />
-            ))}
-          </div>
-        </div>
-        <div className="flex flex-col gap-y-1">
-          <h4>ポイントについて</h4>
-          <div>1回のコードレビュー or 追加の質問で 1pt を消費します。</div>
-        </div>
+      </div>
+      <div className="flex flex-col gap-y-1">
+        <h4>ポイントについて</h4>
+        <div>1回のコードレビュー or 追加の質問で 1pt を消費します。</div>
       </div>
     </div>
   );
