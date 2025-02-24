006257e16ff4ef2e900f57e7c220f5f4eeb84888
shuheitachikawa <shuhei.tusx@gmail.com>
2025-02-24 09:00:43 +0900

    s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const notSansJp = Noto_Sans_JP({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "JS学習サイト(プロトタイプ)",
@@ -25,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-[#f5f8fa] antialiased`}
-      >
+      <body className={`${notSansJp.className} bg-[#f5f8fa] antialiased`}>
         {children}
       </body>
     </html>
