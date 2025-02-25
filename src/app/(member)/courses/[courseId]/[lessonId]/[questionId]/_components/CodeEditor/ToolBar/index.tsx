e2624096dba17dc9a2d2197f3e4b86da7958f6a0
shuheitachikawa <shuhei.tusx@gmail.com>
2025-02-25 13:26:17 +0900

    s

--- src/app/(member)/courses/[courseId]/[lessonId]/[questionId]/_components/CodeEditor/ToolBar/index.tsx
+++ src/app/(member)/courses/[courseId]/[lessonId]/[questionId]/_components/CodeEditor/ToolBar/index.tsx
@@ -1,7 +1,9 @@
+import { faPaperPlane, faPlay } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useParams } from "next/navigation";
 import React from "react";
 import toast from "react-hot-toast";
-import { useMessages } from "../../_hooks/useMessages";
+import { useMessages } from "../../../_hooks/useMessages";
 import { useQuestion } from "@/app/_hooks/useQuestion";
 import { api } from "@/app/_utils/api";
 import { CodeReviewRequest } from "@/app/api/questions/[questionId]/code_review/_types/CodeReview";
@@ -58,9 +60,26 @@ export const ToolBar: React.FC<Props> = ({ answer, onExecuteCode }) => {
 
   return (
     <div className="absolute bottom-4 right-4 flex gap-4 rounded-full border border-gray-700 bg-black px-6 py-4 text-white">
-      <button onClick={saveDraft}>下書き保存</button>
-      <button onClick={review}>提出</button>
-      <button onClick={onExecuteCode}>実行</button>
+      <button
+        type="button"
+        onClick={onExecuteCode}
+        className="flex items-center gap-2 rounded-full bg-gray-400 px-4 py-[10px] text-sm font-bold text-textMain"
+      >
+        <span>コードを実行</span>
+        <FontAwesomeIcon icon={faPlay} className="size-3" />
+      </button>
+      <button
+        type="button"
+        onClick={review}
+        className="flex items-center gap-2 rounded-full bg-blue-500 px-4 py-[10px] text-sm font-bold"
+      >
+        <span>提出してレビューを受ける</span>
+        <FontAwesomeIcon icon={faPaperPlane} className="size-3" />
+      </button>
+      <button type="button" onClick={saveDraft} className="text-sm">
+        下書き保存
+      </button>
+      {/* <EllipsisButton /> */}
     </div>
   );
 };
