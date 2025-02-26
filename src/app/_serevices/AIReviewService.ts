4e5194e188cbd36f6715d128d2f03d19d76c2984
shuheitachikawa <shuhei.tusx@gmail.com>
2025-02-27 08:32:45 +0900

    s

--- src/app/_serevices/AIReviewService.ts
+++ src/app/_serevices/AIReviewService.ts
@@ -16,7 +16,7 @@ export class AIReviewService {
     comments: z.array(
       z.object({
         targetCode: z.string(),
-        level: z.enum(["INFO", "WARNING", "ERROR"]),
+        level: z.enum(["GOOD", "WARNING", "ERROR"]),
         message: z.string(),
       })
     ),
