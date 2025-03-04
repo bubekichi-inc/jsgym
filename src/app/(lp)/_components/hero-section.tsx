7efe7f678aa7d043efd1e77b14c09af788fe3af3
shuheitachikawa <shuhei.tusx@gmail.com>
2025-03-04 14:26:03 +0900

    s

--- src/app/(lp)/_components/hero-section.tsx
+++ src/app/(lp)/_components/hero-section.tsx
@@ -10,8 +10,8 @@ export function HeroSection() {
     <section className="mx-auto max-w-screen-xl py-20 md:py-28">
       <div className="container mx-auto px-4 md:px-6">
         <div className="items-center justify-center gap-10 space-y-6 lg:flex">
-          <div className="flex flex-col justify-center space-y-4 lg:w-1/2">
-            <div className="space-y-2">
+          <div className="flex flex-col justify-center space-y-4 md:space-y-6 lg:w-1/2">
+            <div className="space-y-4">
               <h1 className="space-y-2 whitespace-nowrap text-3xl font-bold sm:text-4xl md:text-5xl lg:text-6xl/none">
                 <div className="">JavaScript開発の</div>
                 <div className="">自走力を鍛える</div>
@@ -23,7 +23,7 @@ export function HeroSection() {
             </div>
             <div className="flex flex-col gap-2 min-[400px]:flex-row">
               <button
-                className="inline-flex h-10 items-center justify-center rounded-md bg-yellow-400 px-4 py-2 text-sm font-bold transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2"
+                className="inline-flex items-center justify-center rounded-md bg-yellow-400 p-4 py-2 text-base font-bold transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 md:px-6 md:py-3 md:text-2xl"
                 onClick={() => setShowLoginDialog(true)}
               >
                 無料ではじめる
