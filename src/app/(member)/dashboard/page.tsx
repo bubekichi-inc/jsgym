d929dae2377254ca6f2ea67090d09091b012119a
shuheitachikawa <shuhei.tusx@gmail.com>
2025-03-07 00:04:53 +0900

    s

--- src/app/(member)/dashboard/page.tsx
+++ src/app/(member)/dashboard/page.tsx
@@ -187,10 +187,13 @@ export default function Dashboard() {
 
   function formatDate(dateString: string) {
     const date = new Date(dateString);
-    return date.toLocaleDateString("ja-JP", {
+    return date.toLocaleString("ja-JP", {
       year: "numeric",
       month: "2-digit",
       day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
     });
   }
 
