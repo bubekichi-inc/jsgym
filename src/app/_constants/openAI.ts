a65b384dabc37cc0830f69417a4e796a5280aa41
shuheitachikawa <shuhei.tusx@gmail.com>
2025-03-07 00:22:16 +0900

    s

--- src/app/_constants/openAI.ts
+++ src/app/_constants/openAI.ts
@@ -1,4 +1,2 @@
 export const GPT_4_5 = "gpt-4.5-preview-2025-02-27";
 export const GPT_4O_MINI = "gpt-4o-mini-2024-07-18";
-// 4.5: "gpt-4.5-preview-2025-02-27";
-// 4o-mini: "gpt-4o-mini-2024-07-18";
