1f31d72b015b8657728fb792e144886d9a1c9722
shuheitachikawa <shuhei.tusx@gmail.com>
2025-03-08 13:18:18 +0900

    s

--- src/app/api/crons/generate_questions/route.ts
+++ src/app/api/crons/generate_questions/route.ts
@@ -13,7 +13,7 @@ import { buildPrisma } from "@/app/_utils/prisma";
  * @returns {("BASIC"|"ADVANCED"|"REAL_WORLD")} ランダムに選ばれた難易度
  */
 const getRandomDifficulty = (): QuestionLevel => {
-  const basicRate = 0.5;
+  const basicRate = 0.4;
   const advancedRate = 0.3;
 
   const random = Math.random();
