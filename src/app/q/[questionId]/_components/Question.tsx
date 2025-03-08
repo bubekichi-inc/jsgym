e3a1e7a94644485c097bca17921de70f49b0ddb2
shuheitachikawa <shuhei.tusx@gmail.com>
2025-03-08 12:08:08 +0900

    s

--- src/app/q/[questionId]/_components/Question.tsx
+++ src/app/q/[questionId]/_components/Question.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { faCopy } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import { useParams } from "next/navigation";
 import React, { useState } from "react";
+import { toast } from "react-toastify";
 import { ReviewerModal } from "./ReviewerModal";
 import { Skeleton } from "@/app/_components/Skeleton";
 import { useQuestion } from "@/app/_hooks/useQuestion";
@@ -14,6 +17,17 @@ export const Question: React.FC = () => {
   const { data } = useQuestion({
     questionId,
   });
+
+  const copyToClipboard = (text: string) => {
+    navigator.clipboard.writeText(text).then(
+      () => {
+        toast.success("コピーしました");
+      },
+      (err) => {
+        toast.error("コピー失敗:", err);
+      }
+    );
+  };
 
   if (!data) return <Skeleton height={300} />;
 
@@ -42,13 +56,29 @@ export const Question: React.FC = () => {
 
         <div className="space-y-4 rounded-lg bg-orange-50 p-3">
           <div className="space-y-1">
-            <p className="text-xs font-bold">入力例</p>
+            <div className="flex items-center gap-2">
+              <p className="text-xs font-bold">入力例</p>
+              <button
+                onClick={() => copyToClipboard(data.question.inputCode)}
+                className="text-xs"
+              >
+                <FontAwesomeIcon icon={faCopy} className="mr-1" />
+              </button>
+            </div>
             <div className="whitespace-pre-wrap break-words text-xs">
               {data.question.inputCode}
             </div>
           </div>
           <div className="space-y-1">
-            <p className="text-xs font-bold">出力例</p>
+            <div className="flex items-center gap-2">
+              <p className="text-xs font-bold">出力例</p>
+              <button
+                onClick={() => copyToClipboard(data.question.outputCode)}
+                className="text-xs"
+              >
+                <FontAwesomeIcon icon={faCopy} className="mr-1" />
+              </button>
+            </div>
             <div className="whitespace-pre-wrap break-words text-xs">
               {data.question.outputCode}
             </div>
