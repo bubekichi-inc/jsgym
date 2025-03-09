a1d2fda82ae9499b88c1949c21dfdb5c1bd6dd84
s-yamauchi07 <hand1nhand306@gmail.com>
2025-03-09 16:22:54 +0900

    型の変更

--- src/app/api/notifications/_types/notification.ts
+++ src/app/api/notifications/_types/notification.ts
@@ -1,5 +1,11 @@
-export type Notification = {
+export type NotificationRequest = {
   receiveNewQuestionNotification: boolean;
   receiveUsefulInfoNotification: boolean;
   receiveReminderNotification: boolean;
 };
+
+export type NotificationResponse = {
+  receiveNewQuestionNotification: boolean;
+  receiveUsefulInfoNotification: boolean;
+  receiveReminderNotification: boolean;
+} | undefined;
