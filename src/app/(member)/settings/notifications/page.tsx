0c5e1bdb25301abb5460ed6f9c7c9021b3df445c
s-yamauchi07 <hand1nhand306@gmail.com>
2025-03-09 16:22:42 +0900

    fetchとupdate処理の実装

--- src/app/(member)/settings/notifications/page.tsx
+++ src/app/(member)/settings/notifications/page.tsx
@@ -1,8 +1,93 @@
 "use client";
+
 import React from "react";
+import { useNotifications } from "./_hooks/useNotifications";
+import { api } from "@/app/_utils/api";
+import { NotificationRequest, NotificationResponse } from "@/app/api/notifications/_types/notification";
+
+type NotificationSettingKey =
+  "receiveNewQuestionNotification"
+  | "receiveReminderNotification"
+  | "receiveUsefulInfoNotification"
 
 const Page: React.FC = () => {
-  return <div className="">通知設定ページ</div>;
+  const { error, data } = useNotifications();
+  const handleChangeSetting = async(selectItem: NotificationSettingKey) => {
+    if(!data) return;
+
+    const updatedData: NotificationRequest = {
+      ...data,
+      [selectItem]: !data[selectItem],
+    }
+
+    try {
+      await api.put<NotificationRequest, NotificationResponse>("/api/notifications", updatedData);      
+    } catch (e) {
+      console.log("通知設定の更新に失敗:", e);
+    }
+  };
+
+  if (!data) return <p>読み込み中...</p>;
+  if (error)
+    return <p className="text-red-500">データ取得に失敗: {error.message}</p>;
+
+  return (
+    <>
+      <div className="">
+        通知設定
+      </div>
+
+      <div>
+        <div>
+          <input 
+            id="new-question" 
+            type="checkbox"
+            className="size-4 rounded-sm border-gray-300 bg-gray-100 text-blue-600 focus:ring-blue-500"
+            onClick={() => handleChangeSetting("receiveNewQuestionNotification")}
+            defaultChecked={data.receiveNewQuestionNotification}
+          />
+          <label 
+            htmlFor="new-question"
+            className="ms-2 text-sm font-medium text-gray-900"
+          >
+            新着問題
+          </label>
+        </div>
+
+        <div>
+          <input 
+            id="useful-information" 
+            type="checkbox"
+            className="size-4 rounded-sm border-gray-300 bg-gray-100 text-blue-600 focus:ring-blue-500"
+            onClick={() => handleChangeSetting("receiveUsefulInfoNotification")}
+            defaultChecked={data.receiveUsefulInfoNotification}
+          />
+          <label 
+            htmlFor="useful-information"
+            className="ms-2 text-sm font-medium text-gray-900"
+          >
+            お役立ち情報
+          </label>
+        </div>
+
+        <div>
+          <input 
+            id="reminder" 
+            type="checkbox"
+            className="size-4 rounded-sm border-gray-300 bg-gray-100 text-blue-600 focus:ring-blue-500"
+            onClick={() => handleChangeSetting("receiveReminderNotification")}
+            defaultChecked={data.receiveReminderNotification}
+          />
+          <label 
+            htmlFor="reminder"
+            className="ms-2 text-sm font-medium text-gray-900"
+          >
+            リマインダー&その他
+          </label>
+        </div>
+      </div>
+    </>
+  )
 };
 
 export default Page;
