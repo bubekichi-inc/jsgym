77f0cba59966e086752927c3f4a6ef28f6a27522
shuheitachikawa <shuhei.tusx@gmail.com>
2025-03-10 14:50:12 +0900

    s

--- src/app/q/_components/Questions.tsx
+++ src/app/q/_components/Questions.tsx
@@ -153,7 +153,7 @@ export const Questions: React.FC<Props> = ({ limit }) => {
                     }`}
                     onClick={() => handleStatusChange("ALL")}
                   >
-                    全て
+                    すべて
                   </button>
                   <button
                     className={`inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-400 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 ${
