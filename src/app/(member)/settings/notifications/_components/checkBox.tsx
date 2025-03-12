efd5681f0bce3778617b648d98b3f84fe542bc1c
s-yamauchi07 <hand1nhand306@gmail.com>
2025-03-12 21:54:29 +0900

    不要な属性の削除

--- src/app/(member)/settings/notifications/_components/checkBox.tsx
+++ src/app/(member)/settings/notifications/_components/checkBox.tsx
@@ -3,32 +3,21 @@ import type { ComponentPropsWithRef } from "react";
 
 interface Props extends ComponentPropsWithRef<"input"> {
   label: string;
-  onClick? : () => Promise<void>;
 }
 
-export const CheckBox: React.FC<Props> = ({ 
-  label,
-  id,
-  type,
-  defaultChecked,
-  disabled,
-  onClick,
-  ...props
-  }) => {
-    return (
-      <div className="flex items-center">
-        <input
-          id={id} 
-          type={type}
-          className="size-5 rounded-sm border-gray-300 bg-buttonMain text-blue-600"
-          onClick={onClick}
-          defaultChecked={defaultChecked}
-          disabled={disabled}
-          {...props}
-        />
-        <label htmlFor="id" className="ms-2 text-lg font-bold text-gray-900">
-          {label}
-        </label>
-      </div>
-    );
-}
+export const CheckBox: React.FC<Props> = ({ label, ...props }) => {
+  return (
+    <div className="flex items-center">
+      <input
+        className="size-5 rounded-sm border-gray-300 bg-buttonMain text-blue-600"
+        {...props}
+      />
+      <label
+        htmlFor={props.id}
+        className="ms-2 text-lg font-bold text-gray-900"
+      >
+        {label}
+      </label>
+    </div>
+  );
+};
