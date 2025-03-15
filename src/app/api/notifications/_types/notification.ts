4c49e96cab752d9bc7cbf124a21ee935920b4b77
s-yamauchi07 <hand1nhand306@gmail.com>
2025-03-16 00:03:06 +0900

    UpdateNotificationRequestの型修正

--- src/app/api/notifications/_types/notification.ts
+++ src/app/api/notifications/_types/notification.ts
@@ -4,10 +4,11 @@ export type FetchNotificationRequest = {
   receiveReminderNotification: boolean;
 }
 
-export type UpdateNotificationRequest = 
-  | { receiveNewQuestionNotification: boolean }
-  | { receiveUsefulInfoNotification: boolean }
-  | { receiveReminderNotification: boolean };
+export type UpdateNotificationRequest = {
+  receiveNewQuestionNotification?: true,
+  receiveUsefulInfoNotification?: true,
+  receiveReminderNotification?: true,
+};
 
 export type NotificationSettingKey =
 | "receiveNewQuestionNotification"
