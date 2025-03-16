14aec45a450281891754be8dc5dc08a108817aad
shuheitachikawa <shuhei.tusx@gmail.com>
2025-03-16 14:11:25 +0900

    s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
             })(window, document, "clarity", "script", ${clarityProjectId});
-        `}
+          `}
         </Script>
       )}
     </html>
