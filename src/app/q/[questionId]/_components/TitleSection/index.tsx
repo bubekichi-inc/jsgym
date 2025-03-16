c4dd1cdd17d91b210ceb7156b0b934dac7616234
shuheitachikawa <shuhei.tusx@gmail.com>
2025-03-17 00:08:25 +0900

    s

--- src/app/q/[questionId]/_components/TitleSection/index.tsx
+++ src/app/q/[questionId]/_components/TitleSection/index.tsx
@@ -22,34 +22,29 @@ export const TitleSection: React.FC = () => {
 
   return (
     <>
-      <div className="flex items-center justify-between gap-4">
-        <div className="items-start space-y-2 md:flex md:items-center md:gap-6 md:space-y-0">
-          <div className="flex items-center gap-1">
-            <span
-              className={`whitespace-nowrap rounded-full px-2 py-1 text-xs font-bold text-white ${
-                lessonStyleMap[
-                  data.question.lesson.id as keyof typeof lessonTextMap
-                ]
-              }`}
-            >
-              {
-                lessonTextMap[
-                  data.question.lesson.id as keyof typeof lessonTextMap
-                ]
-              }
-            </span>
-            <span className="text-xs text-gray-600 md:text-sm">
-              {data.question.id}
-            </span>
-          </div>
-          <div className="flex items-center gap-2">
-            <h1 className="text-base font-bold md:text-xl">
-              {data.question.title}
-            </h1>
-            <StatusBadge status={data.userQuestion?.status || null} />
-          </div>
+      <div className="flex items-center justify-between gap-3">
+        <div className="items-start space-y-2 md:flex md:items-center md:gap-3 md:space-y-0">
+          <span
+            className={`whitespace-nowrap rounded-full px-2 py-1 text-xs font-bold text-white ${
+              lessonStyleMap[
+                data.question.lesson.id as keyof typeof lessonTextMap
+              ]
+            }`}
+          >
+            {
+              lessonTextMap[
+                data.question.lesson.id as keyof typeof lessonTextMap
+              ]
+            }
+          </span>
+          <h1 className="text-base font-bold md:text-lg">
+            {data.question.title}
+          </h1>
         </div>
         <div className="flex flex-col items-center gap-1 md:flex-row">
+          <div className="px-1">
+            <StatusBadge status={data.userQuestion?.status || null} />
+          </div>
           <BookmarkButton />
           <DropdownMenu onShowAnswer={() => setShowAnswerModal(true)} />
         </div>
