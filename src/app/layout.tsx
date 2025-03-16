0e0205008878c206e8eb52c3a59abc1d71a1c649
shuheitachikawa <shuhei.tusx@gmail.com>
2025-03-16 15:14:07 +0900

    s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,21 +53,17 @@ export default function RootLayout({
         </a>
       </body>
       {gaId && <GoogleAnalytics gaId={gaId} />}
-      {/* {clarityProjectId && (
-        <Script
-          id="clarity-script"
-          strategy="afterInteractive"
-          type="text/javascript"
-        >
+      {clarityProjectId && (
+        <Script id="clarity-script" strategy="afterInteractive">
           {`
             (function(c,l,a,r,i,t,y){
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", ${clarityProjectId});
-          `}
+            })(window, document, "clarity", "script", "${clarityProjectId}");
+        `}
         </Script>
-      )} */}
+      )}
     </html>
   );
 }
