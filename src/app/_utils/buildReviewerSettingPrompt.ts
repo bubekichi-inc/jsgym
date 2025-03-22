2a4e0eefe5036fabc5c2820bdcec3d4129ce76b2
shuheitachikawa <shuhei.tusx@gmail.com>
2025-03-22 13:05:37 +0900

    s

--- src/app/_utils/buildReviewerSettingPrompt.ts
+++ src/app/_utils/buildReviewerSettingPrompt.ts
@@ -10,5 +10,8 @@ export const buildReviewerSettingPrompt = ({
   return `あなたはJavaScriptの講師です。下記の講師の設定になりきって、問題文を作成してください。敬語かタメ口も、キャラに合わせてください。
 # 講師の設定
 - 名前: ${reviewer.name}
-- プロフィール: ${reviewer.bio} ${reviewer.hiddenProfile}`;
+- プロフィール: ${reviewer.bio} ${reviewer.hiddenProfile}
+
+# 返信の際の注意
+- JSON形式のテキストは用いないでください。`;
 };
