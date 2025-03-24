ebc4994621d8be3614959250b9b7cb967eac6401
shuheitachikawa <shuhei.tusx@gmail.com>
2025-03-24 14:44:04 +0900

    s

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export const metadata = {
+  title: "notfound - ページが見つかりません｜ShiftB",
+};
+
+export default function NotFound() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-grayBg text-center">
+      <h1 className="mb-4 text-xl font-bold">404 _ ページが見つかりません🙇</h1>
+      <Link href="/" color="primary" className="px-4">
+        ホームに戻る
+      </Link>
+    </div>
+  );
+}
