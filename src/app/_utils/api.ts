370d1064ee60f5fdf4b1f239079639da6efcf296
shuheitachikawa <shuhei.tusx@gmail.com>
2025-03-24 22:58:57 +0900

    s

--- src/app/_utils/api.ts
+++ src/app/_utils/api.ts
@@ -10,7 +10,7 @@ export const api = axios.create({
     "Content-Type": "application/json",
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers":
-      "Content-Type, Authorization, X-Requested-With",
+      "Content-Type, Authorization",
   },
 });
 
