e3a984ed58e567df174942901705c2b782cd6683
shuheitachikawa <shuhei.tusx@gmail.com>
2025-03-24 22:40:03 +0900

    s

--- src/app/_utils/api.ts
+++ src/app/_utils/api.ts
@@ -7,6 +7,9 @@ export const api = axios.create({
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers":
+      "Content-Type, Authorization, X-Requested-With",
   },
 });
 
