a05871972e1a0d76fbc35d3f456fa772fc2fe18c
shuheitachikawa <shuhei.tusx@gmail.com>
2025-04-07 19:38:16 +0900

    s

--- next.config.ts
+++ next.config.ts
@@ -67,6 +67,11 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "ulznrmsnhzwzqdxjhqbx.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+      {
+        protocol: "https",
         hostname: "placehold.jp",
       },
       {
