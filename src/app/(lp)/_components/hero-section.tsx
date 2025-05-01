182184363b01b37809d587a4303ba84175d1237d
hayato33 <country.devide1933@gmail.com>
2025-05-01 15:43:28 +0900

    ヒーローセクションのデザイン変更

--- src/app/(lp)/_components/hero-section.tsx
+++ src/app/(lp)/_components/hero-section.tsx
@@ -1,49 +1,60 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import { SinginModal } from "@/app/_components/SinginModal";
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+import { SinginModal } from '@/app/_components/SinginModal';
 
 export function HeroSection() {
   const [showLoginDialog, setShowLoginDialog] = useState(false);
 
   return (
-    <section className="mx-auto max-w-screen-xl py-20 md:py-28">
-      <div className="container mx-auto px-4 md:px-6">
-        <div className="items-center justify-center gap-14 space-y-6 lg:flex">
-          <div className="flex flex-col justify-center space-y-4 md:space-y-6 lg:w-1/2">
-            <div className="space-y-4">
-              <h1 className="space-y-1 whitespace-nowrap text-3xl font-bold sm:text-4xl md:space-y-3 md:text-5xl">
-                <div className="">JavaScriptの</div>
-                <div className="">自走力を鍛える</div>
-                <div className="">トレーニングジム</div>
-              </h1>
-              <p className="max-w-[600px] text-gray-500 md:text-lg">
-                JavaScriptに特化した、プログラミング学習サイトです。
-              </p>
-            </div>
-            <div className="flex flex-col gap-2 min-[400px]:flex-row">
-              <button
-                className="inline-flex items-center justify-center rounded-md bg-yellow-400 p-4 py-2 text-base font-bold transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 md:px-5 md:py-2 md:text-lg"
-                onClick={() => setShowLoginDialog(true)}
-              >
-                無料ではじめる
-              </button>
-            </div>
+    <section className='px-6 py-8 md:py-16'>
+      <div className='mx-auto flex max-w-fit flex-col items-start justify-center md:flex-row md:items-center md:gap-x-16 md:rounded-2xl md:bg-white md:px-[72px] md:py-[100px] md:shadow-card'>
+        <div className='contents md:flex md:max-w-96 md:flex-col md:justify-center'>
+          <h1 className='text-4xl/normal font-bold md:text-5xl/[1.3]'>
+            <div>JavaScriptの</div>
+            <div>自走力を鍛える</div>
+            <div>トレーニングジム</div>
+          </h1>
+          <p className='mt-3 text-lg/[1.5] text-gray-500 md:text-xl/[1.5]'>
+            JavaScriptに特化した、プログラミング学習サイトです。
+          </p>
+          <div className='order-4 mt-[30px] flex w-full flex-col gap-4 md:order-none md:mt-6 md:gap-6'>
+            <button
+              className='inline-flex h-[68px] w-full items-center justify-center gap-1.5 rounded-xl bg-yellow-400 px-4 text-xl font-bold transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 md:gap-3 md:px-5 md:text-[1.625rem]'
+              onClick={() => setShowLoginDialog(true)}
+            >
+              無料ではじめる
+              <Image
+                src='/images/icon_black.svg'
+                alt='logo'
+                width={28}
+                height={32}
+                className='h-auto w-6 md:w-7'
+              />
+            </button>
+            <Link
+              href='/about'
+              className='inline-flex h-[68px] w-full items-center justify-center rounded-xl bg-buttonMain px-4 text-xl font-bold text-white transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 md:px-5 md:text-[1.625rem]'
+            >
+              JS Gymについてくわしく
+            </Link>
           </div>
-          <div className="z-[-1] flex items-center justify-center">
-            <div className="overflow-hidden rounded-lg border bg-gray-50 shadow-card">
-              <video
-                className="size-full object-cover"
-                src="/images/demo.mov"
-                controls={false}
-                autoPlay
-                muted
-                loop
-                playsInline
-              >
-                お使いのブラウザは動画再生をサポートしていません。
-              </video>
-            </div>
+        </div>
+        <div className='mt-8 flex max-w-[560px] items-center justify-center md:mt-0'>
+          <div className='overflow-hidden rounded-lg border bg-gray-50 shadow-card'>
+            <video
+              className='size-full object-cover'
+              src='/images/demo.mov'
+              controls={false}
+              autoPlay
+              muted
+              loop
+              playsInline
+            >
+              お使いのブラウザは動画再生をサポートしていません。
+            </video>
           </div>
         </div>
       </div>
