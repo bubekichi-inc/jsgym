09f4f7edcbc8834955033890a89f445e32d5cced
hayato33 <country.devide1933@gmail.com>
2025-05-04 19:34:22 +0900

    表示する問題数を12件に統一

--- src/app/(lp)/page.tsx
+++ src/app/(lp)/page.tsx
@@ -1,17 +1,13 @@
-'use client';
-
-import { useDevice } from '../_hooks/useDevice';
 import { Footer } from './_components/footer';
 import { Questions } from '@/app/(lp)/_components/Questions';
 import { HeroSection } from '@/app/(lp)/_components/hero-section';
 
 export default function Home() {
-  const { isSp } = useDevice();
   return (
     <div className='flex min-h-screen flex-col'>
       <main className='flex-1'>
         <HeroSection />
-        {isSp ? <Questions limit={9} /> : <Questions limit={12} />}
+        <Questions limit={12} />
       </main>
       <Footer />
     </div>
