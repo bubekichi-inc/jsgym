07dc89fcb343c2b4ef6b0eb9e65e32ba141ddea0
hayato33 <country.devide1933@gmail.com>
2025-05-05 17:24:10 +0900

    ブックマークアイコンと問題ステータスの位置調整

--- src/app/_components/QuestionCard.tsx
+++ src/app/_components/QuestionCard.tsx
@@ -1,17 +1,17 @@
-'use client';
+"use client";
 
-import { faBookmark } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import dayjs from 'dayjs';
-import Image from 'next/image';
-import Link from 'next/link';
+import { faBookmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import dayjs from "dayjs";
+import Image from "next/image";
+import Link from "next/link";
 import {
   questionTagTextMap,
   questionTypeTextMap,
   userQuestionStatusColorMap,
   userQuestionStatusTextMap,
-} from '@/app/_constants';
-import { Question } from '@/app/api/questions/route';
+} from "@/app/_constants";
+import { Question } from "@/app/api/questions/route";
 
 interface QuestionCardProps {
   question: Question;
@@ -20,21 +20,21 @@ interface QuestionCardProps {
 export const QuestionCard: React.FC<QuestionCardProps> = ({ question }) => {
   const status = question.userQuestions?.[0]?.status || null;
   return (
-    <div className='flex flex-col gap-y-4 rounded-2xl bg-white p-6 shadow-blue'>
-      <div className='flex justify-between'>
-        <div className='flex flex-col gap-y-2'>
-          <div className='flex items-center gap-x-2'>
-            <span className='text-xs/[1.5] text-textMain'>
-              {dayjs(question.createdAt).format('YYYY/MM/DD_HH:mm')}
+    <div className="flex flex-col gap-y-4 rounded-2xl bg-white p-6 shadow-blue">
+      <div className="flex justify-between">
+        <div className="flex flex-col gap-y-2">
+          <div className="flex items-center gap-x-2">
+            <span className="text-xs/[1.5] text-textMain">
+              {dayjs(question.createdAt).format("YYYY/MM/DD_HH:mm")}
             </span>
-            {dayjs(question.createdAt).isSame(dayjs(), 'day') && (
-              <span className='text-sm/[1] font-bold text-yellow-400'>
+            {dayjs(question.createdAt).isSame(dayjs(), "day") && (
+              <span className="text-sm/[1] font-bold text-yellow-400">
                 NEW!!
               </span>
             )}
           </div>
-          <div className='flex items-center gap-x-[19px]'>
-            <span className='relative bg-buttonMain py-1.5 pl-5 pr-1 text-base/[1] font-bold text-white before:absolute before:left-2 before:top-1/2 before:size-1 before:-translate-y-1/2 before:rounded-l-full before:bg-white after:absolute after:right-[-9px] after:top-0 after:block after:h-full after:w-[9px] after:bg-buttonMain after:[clip-path:polygon(9px_50%,_0%_0%,_0%_100%)]'>
+          <div className="flex items-center gap-x-[19px]">
+            <span className="relative bg-buttonMain py-1.5 pl-5 pr-1 text-base/[1] font-bold text-white before:absolute before:left-2 before:top-1/2 before:size-1 before:-translate-y-1/2 before:rounded-l-full before:bg-white after:absolute after:right-[-9px] after:top-0 after:block after:h-full after:w-[9px] after:bg-buttonMain after:[clip-path:polygon(9px_50%,_0%_0%,_0%_100%)]">
               {
                 questionTypeTextMap[
                   question.type as keyof typeof questionTypeTextMap
@@ -42,24 +42,24 @@ export const QuestionCard: React.FC<QuestionCardProps> = ({ question }) => {
               }
             </span>
             {question.reviewer && (
-              <div className='group relative'>
+              <div className="group relative">
                 <div
                   className={`flex size-10 items-center justify-center overflow-hidden rounded-full`}
                 >
                   <Image
                     src={question.reviewer.profileImageUrl}
-                    alt='reviewer'
+                    alt="reviewer"
                     width={80}
                     height={80}
-                    className='size-full object-cover'
+                    className="size-full object-cover"
                   />
                 </div>
-                <div className='invisible absolute -right-4 bottom-full z-10 mb-2 w-[320px] rounded-lg bg-gray-900 p-2 text-sm text-white opacity-0 transition-all group-hover:visible group-hover:opacity-100'>
-                  <div className='flex items-center gap-2'>
-                    <span className='text-xs text-gray-300'>レビュワー</span>
-                    <p className='font-bold'>{question.reviewer.name}</p>
+                <div className="invisible absolute -right-4 bottom-full z-10 mb-2 w-[320px] rounded-lg bg-gray-900 p-2 text-sm text-white opacity-0 transition-all group-hover:visible group-hover:opacity-100">
+                  <div className="flex items-center gap-2">
+                    <span className="text-xs text-gray-300">レビュワー</span>
+                    <p className="font-bold">{question.reviewer.name}</p>
                   </div>
-                  <p className='mt-1 whitespace-pre-line text-xs text-gray-300'>
+                  <p className="mt-1 whitespace-pre-line text-xs text-gray-300">
                     {question.reviewer.bio}
                   </p>
                 </div>
@@ -67,7 +67,10 @@ export const QuestionCard: React.FC<QuestionCardProps> = ({ question }) => {
             )}
           </div>
         </div>
-        <div className='flex gap-4'>
+        <div className="flex gap-4">
+          {question.questionBookmarks?.length > 0 && (
+            <FontAwesomeIcon icon={faBookmark} className="text-yellow-500" />
+          )}
           {status && (
             <span
               className={`inline-flex h-12 w-20 items-center justify-center rounded-md text-base/[1] font-bold ${userQuestionStatusColorMap[status]}`}
@@ -75,24 +78,21 @@ export const QuestionCard: React.FC<QuestionCardProps> = ({ question }) => {
               {userQuestionStatusTextMap[status]}
             </span>
           )}
-          {question.questionBookmarks?.length > 0 && (
-            <FontAwesomeIcon icon={faBookmark} className='text-yellow-500' />
-          )}
         </div>
       </div>
-      <div className='grid gap-2'>
-        <p className='text-lg/[1.5] font-bold md:text-xl/[1.5]'>
+      <div className="grid gap-2">
+        <p className="text-lg/[1.5] font-bold md:text-xl/[1.5]">
           {question.title}
         </p>
-        <p className='line-clamp-2 text-sm/[1.5] md:text-base'>
+        <p className="line-clamp-2 text-sm/[1.5] md:text-base">
           {question.content}
         </p>
       </div>
-      <ul className='flex flex-wrap gap-2'>
+      <ul className="flex flex-wrap gap-2">
         {question.questions.map((q, i) => (
           <li
             key={`${q.tag.name}-${i}`}
-            className='rounded-md border border-gray-500 px-1.5 py-[3px] text-xs/[1] text-gray-500'
+            className="rounded-md border border-gray-500 px-1.5 py-[3px] text-xs/[1] text-gray-500"
           >
             {questionTagTextMap[q.tag.name as keyof typeof questionTagTextMap]}
           </li>
@@ -100,7 +100,7 @@ export const QuestionCard: React.FC<QuestionCardProps> = ({ question }) => {
       </ul>
       <Link
         href={`/q/${question.id}`}
-        className='mt-auto flex h-10 w-full items-center justify-center rounded-md bg-buttonMain text-base/[1] font-bold text-white duration-200 hover:scale-105'
+        className="mt-auto flex h-10 w-full items-center justify-center rounded-md bg-buttonMain text-base/[1] font-bold text-white duration-200 hover:scale-105"
       >
         この問題に挑戦する
       </Link>
