18247dfabfe8dcc8b2d496c274a185d401cfe7f4
hayato33 <country.devide1933@gmail.com>
2025-05-05 16:48:10 +0900

    HeroSectionコンポーネントのファイル名変更

--- src/app/(lp)/page.tsx
+++ src/app/(lp)/page.tsx
@@ -1,11 +1,11 @@
-import { Footer } from '../_components/footer';
-import { Questions } from '@/app/(lp)/_components/Questions';
-import { HeroSection } from '@/app/(lp)/_components/hero-section';
+import { Footer } from "../_components/footer";
+import { HeroSection } from "@/app/(lp)/_components/HeroSection";
+import { Questions } from "@/app/(lp)/_components/Questions";
 
 export default function Home() {
   return (
-    <div className='flex min-h-screen flex-col'>
-      <main className='flex-1'>
+    <div className="flex min-h-screen flex-col">
+      <main className="flex-1">
         <HeroSection />
         <Questions limit={12} />
       </main>
